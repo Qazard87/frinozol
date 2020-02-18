759c6c549d0e50cd7316869efe83377d350ba9c7
Galaktionov Andrey <funroger2008@yandex.ru>
2020-02-18 16:21:29 +0300

    fixed the flickering menu button

--- js/custom/menu-btn.js
+++ js/custom/menu-btn.js
@@ -6,24 +6,26 @@ new Vue({
         isOpen: false,
         icon: "fa-bars",
         visible: 'd-none',
-        elem: document.querySelector('.dropdown')
+
     },
     methods: {
         checkBtnClass: function () {
-            if (this.isOpen) {
-                this.isOpen = false;
-                this.icon = "fa-bars";
-                this.visible = 'd-none';
-            } else {
-                this.isOpen = true;
+            let elem = document.querySelector('.dropdown-menu');
+
+            if (elem.classList.contains('d-none')) {
+                elem.classList.remove('d-none');
+                elem.classList.add('d-block');
                 this.icon = "fa-times";
-                this.visible = 'd-block';
+            } else {
+                elem.classList.remove('d-block');
+                elem.classList.add('d-none');
+                this.icon = "fa-bars";
             }
         },
     },
-    mounted(){
-        let  elements = document.querySelectorAll('.dropdown-item');
-        for(let i = 0; i< elements.length; i++){
+    mounted() {
+        let elements = document.querySelectorAll('.dropdown-item');
+        for (let i = 0; i < elements.length; i++) {
             let current = elements[i];
             current.addEventListener('click', this.checkBtnClass);
         }
